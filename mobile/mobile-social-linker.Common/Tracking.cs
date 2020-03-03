8ca545b6d34429ed8d5865981367a626dd906da9
Maxime Rouiller <maxime.rouiller@microsoft.com>
2020-03-03 13:54:02 -0500

    adding /mobile

---
+++ mobile/mobile-social-linker.Common/Tracking.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace mobile_social_linker.Common
+{
+    public static class Tracking
+    {
+        private static Regex[] DefaultDomainsRegex = new[] {
+            new Regex("(.*\\.)?microsoft\\.com$", RegexOptions.IgnoreCase),
+            new Regex("(.*\\.)?msdn\\.com$", RegexOptions.IgnoreCase),
+            new Regex("(.*\\.)?visualstudio\\.com$", RegexOptions.IgnoreCase)
+        };
+
+        public static string AppendTrackingInfo(string link, string eventName, string channel, string alias)
+        {
+            UriBuilder builder = new UriBuilder(link);
+
+            builder = builder.AddTrackingCode(eventName, channel, alias);
+            builder = builder.RemoveLocale();
+            
+            return builder.Uri.ToString();
+        }
+
+        private static UriBuilder RemoveLocale(this UriBuilder builder)
+        {
+            Regex localeMatcher = new Regex("^/\\w{2}-\\w{2}");
+            builder.Path = localeMatcher.Replace(builder.Path, string.Empty);
+            return builder;
+        }
+
+        private static UriBuilder AddTrackingCode(this UriBuilder builder, string eventName, string channel, string alias)
+        {
+            const string trackingName = "WT.mc_id";
+            string trackingCode = $"{eventName}-{channel}-{alias}";
+            
+            //var qs = QueryString.FromUriComponent(builder.Query);
+            var qs = QueryHelpers.ParseQuery(builder.Query);
+            qs.Remove(trackingName);
+            qs.Add(trackingName, trackingCode);
+            QueryBuilder queryBuilder = new QueryBuilder(qs.SelectMany(x=> x.Value, (c,v) => new KeyValuePair<string,string>(c.Key, v) ).ToList());
+            builder.Query = queryBuilder.ToQueryString().ToString();
+
+            return builder;
+        }
+    }
+}
---
+++ mobile/mobile-social-linker/mobile-social-linker.Android/RedditShare.cs
@@ -0,0 +1,38 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using mobile_social_linker.Common;
+using Xamarin.Essentials;
+
+namespace mobile_social_linker.Droid
+{
+    [Activity(Label = "RedditShare", Icon = "@mipmap/icon", Theme = "@android:style/Theme.NoDisplay")]
+    [IntentFilter(new[] { Intent.ActionSend }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = @"text/html", Label = "Reddit")]
+    public class RedditShare : Activity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            if (Intent.Action == Intent.ActionSend)
+            {
+
+                // This is just an example of the data stored in the extras 
+                var uriFromExtras = Intent.GetStringExtra(Intent.ExtraText).Trim();
+                var alias = await SecureStorage.GetAsync("Alias");
+
+                // Check for existing query string and use & instead
+                var uri = Tracking.AppendTrackingInfo(uriFromExtras, "social", "reddit", alias);
+
+                await Clipboard.SetTextAsync(uri);
+
+                await Launcher.TryOpenAsync($"https://reddit.com/submit?url={uri}");
+            }
+            Finish();
+        }
+    }
+
+    
+}
