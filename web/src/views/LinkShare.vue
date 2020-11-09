014bd1f3ffe96d47633062d9cfbbd83cb5180643
Shayne Boyer <shayneboyer@Shaynes-MacBook-Pro.local>
2020-11-09 13:40:54 -0500

    fix space in m365 area

--- web/src/views/LinkShare.vue
+++ web/src/views/LinkShare.vue
@@ -118,7 +118,7 @@ export default {
               "iot",
               "java",
               "javascript",
-              "m365 ",
+              "m365",
               "mobile",
               "modinfra",
               "modops",
