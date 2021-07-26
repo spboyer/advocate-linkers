21e1734656460f50aff05bb4ff5254d51a66a3cb
Shayne Boyer <spboyer@live.com>
2021-07-26 18:31:03 +0000

    add security area selection

--- web/src/views/LinkShare.vue
+++ web/src/views/LinkShare.vue
@@ -126,6 +126,7 @@ export default {
               "power",
               "python",
               "quantum",
+              "security",
               "spatial",
               "startup"
             ]
